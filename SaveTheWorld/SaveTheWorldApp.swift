5b1d68fd9263a69d6830545973091e191f08bd9f
sezg <sezgandhi@gmail.com>
2020-12-28 17:42:50 -0500

    Initial Commit

---
+++ SaveTheWorld/SaveTheWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveTheWorldApp.swift
+//  SaveTheWorld
+//
+//  Created by Sagar Gandhi on 2020-12-28.
+//
+
+import SwiftUI
+
+@main
+struct SaveTheWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
